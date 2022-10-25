2c16e190ebc7b708c45e80b8572afff88c7f08bf
lim5max <44651051+lim5max@users.noreply.github.com>
2022-10-26 00:09:36 +0300

    Add files via upload

---
+++ quest/second.js
@@ -0,0 +1,7 @@
+let elem = document.querySelector('#input')
+
+elem.addEventListener("keyup", (e)=>{
+	if (e.code == 'Enter' && elem.value == '1895'){
+		alert('https://lim5max.github.io/quest/123/123.html')
+	}
+})
